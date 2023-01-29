8e37bf0c3f0c617f6b73999a2fbf9336ebe85c20
Elias Nicolas <sailenicolas@users.noreply.github.com>
2023-01-28 23:41:33 -0300

    Fixed Interceptor

--- src/app/interceptors/httpclient.interceptor.ts
+++ src/app/interceptors/httpclient.interceptor.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,6 +11,13 @@ export class HttpclientInterceptor implements HttpInterceptor {
 	constructor(private auth: AuthService) {}
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		let isApiUrl = req.url.startsWith(environment.urlHost);
+		if (this.auth.isAuthenticated() && isApiUrl) {
+			req = req.clone({
+				setHeaders: { Authorization: `Bearer ${this.auth.currentToken.access_token}` },
+			});
+		}
+
 		return next.handle(req);
 	}
 }
